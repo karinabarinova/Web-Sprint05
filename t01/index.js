d995d6ab53d83f5690f0b0b116fff8cf5ea6263e
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-21 12:16:18 +0200

    updated task

--- t01/index.js
+++ t01/index.js
@@ -4,3 +4,13 @@ exports.var0 = true;
 exports.var1 = `string`;
 exports.var2 = 12.5;
 exports.var3 = 1;
+
+// For test.js testing
+// const i = require('./index');
+
+// console.log(`ironMan    is ${typeof i.ironMan} ${i.ironMan}`);
+// console.log(`warMachine is ${typeof i.warMachine} ${i.warMachine}`);
+// console.log(`var0       is ${typeof i.var0} ${i.var0}`);
+// console.log(`var1       is ${typeof i.var1} ${i.var1}`);
+// console.log(`var2       is ${typeof i.var2} ${i.var2}`);
+// console.log(`var3       is ${typeof i.var3} ${i.var3}`);
